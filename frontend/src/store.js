516e6fe568458d65bf81f8fa0022ceb00bdd3a2a
Siavash Sajady <hubgitpro@gmail.com>
2023-03-07 18:25:10 +0000

    Implemented Product list reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
+
+const reducers = {
+  productList: productListReducer,
+};
+const initialState = {};
+const middleware = [thunk];
 
 const store = configureStore({
-  reducer: {},
-  preloadedState: {},
-  middleware: [thunk],
+  reducer: reducers,
+  preloadedState: initialState,
+  middleware: middleware,
 });
 
 export default store;
