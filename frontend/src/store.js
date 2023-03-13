e1fe3497c2acad374cac35eb94bddb18c1465f73
Siavash Sajady <hubgitpro@gmail.com>
2023-03-13 11:30:24 +0000

    Add userLoginReducer to store.js file for handling user login actions and updating state.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,13 +4,14 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers';
-
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
-const reducers = {
+const reducer = {
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 };
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
@@ -23,7 +24,7 @@ const initialState = {
 const middleware = [thunk];
 
 const store = configureStore({
-  reducer: reducers,
+  reducer: reducer,
   preloadedState: initialState,
   middleware: middleware,
 });
